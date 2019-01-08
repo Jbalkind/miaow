0234c6a9d1acdb14a09ebdaec1f25fb62845bad0
Jonathan Balkind <jbalkind@princeton.edu>
2019-01-07 20:16:37 -0500

    FPGA-specific files going in the fpga folder

---
+++ src/verilog/rtl/fpga/fpga_memory_2.v
@@ -0,0 +1,358 @@
+`include "define.vh"
+
+module fpga_memory_2 # (
+    parameter LSU_MEM_DATA_WIDTH = 32,
+    parameter VGPR_BUS_WIDTH = 2048,
+    parameter VGPR_BUS_WIDTH_LOG = 11,
+    parameter MAX_PKT_LEN = 35,
+    parameter MSG_MSHRID_HI = 13,
+    parameter MSG_MSHRID_LO = 6
+)
+(
+    input wire clk,
+    input wire rst,
+
+    // output to noc2 (send requests to noc2 through packet filter)
+    output reg                          noc2_filter_val,
+    output wire [`NOC_DATA_WIDTH - 1:0] noc2_filter_data,
+    input wire                          filter_noc2_rdy,
+
+    // input from noc3 (receive responses from noc3 through packet filter)
+    input wire                         filter_noc3_val,
+    input wire [`NOC_DATA_WIDTH - 1:0] filter_noc3_data,
+    output reg                         noc3_filter_rdy,
+
+    // LSU inputs/outputs
+    input wire                            lsu_wr_en,
+    input wire                            lsu_rd_en,
+    input wire [LSU_MEM_DATA_WIDTH - 1:0] lsu_addr,
+    input wire [LSU_MEM_DATA_WIDTH - 1:0] lsu_wr_data,
+    input wire [6:0]                      lsu_tag_req,
+
+    output reg  [6:0]                      lsu_tag_resp,
+    output wire [LSU_MEM_DATA_WIDTH - 1:0] lsu_rd_data,
+    output reg                             lsu_ack
+);
+
+// STATES
+localparam STATE_IDLE = 4'd0; // waiting for a mem request from LSU
+localparam STATE_WR_SAVE_DATA = 4'd1; // get 32 bits from LSU
+localparam STATE_WR_WAIT_DATA = 4'd2; // wait a cycle to get data from LSU
+localparam STATE_WR_SLIDE_DATA = 4'd3; // shift data over in buffer
+localparam STATE_WR_SEND_FLIT_ONE = 4'd4; // send first write header flit
+localparam STATE_WR_SEND_FLIT_TWO = 4'd5; // send second write header flit
+localparam STATE_WR_SEND_FLIT_THREE = 4'd6; // send third write header flit
+localparam STATE_WR_SEND_FLIT_DATA = 4'd7; // send data to write
+localparam STATE_WR_ACK_WAIT = 4'd8; // wait for ack from memory
+localparam STATE_RD_SEND_FLIT_ONE = 4'd9; // send first read header flit
+localparam STATE_RD_SEND_FLIT_TWO = 4'd10; // send second read header flit
+localparam STATE_RD_SEND_FLIT_THREE = 4'd11; // send third read header flit
+localparam STATE_RD_RESP_HEAD = 4'd12; // response header from memory
+localparam STATE_RD_RESP_DATA = 4'd13; // response data from memory
+localparam STATE_RD_SEND_DATA = 4'd14; // send data to LSU
+localparam STATE_RD_WAIT_TO_SEND = 4'd15; // wait to send data to LSU
+
+reg [4:0] state;
+reg [4:0] state_next;
+
+// amount of data seen so far (in either read rsp or write req)
+reg [VGPR_BUS_WIDTH_LOG - 1:0] bits_seen;
+reg [VGPR_BUS_WIDTH_LOG - 1:0] bits_seen_next;
+
+reg [5:0] flit_count;
+reg [5:0] flit_count_next;
+
+reg [5:0] total_flits_to_send;
+reg [5:0] total_flits_to_send_next;
+
+reg [5:0] total_flits_to_see;
+reg [5:0] total_flits_to_see_next;
+
+reg [VGPR_BUS_WIDTH - 1:0] data_buffer;
+reg [VGPR_BUS_WIDTH - 1:0] data_buffer_next;
+
+reg [6:0] lsu_tag_resp_next;
+
+reg [6:0] lsu_tag_req_reg;
+reg [6:0] lsu_tag_req_reg_next;
+
+reg [LSU_MEM_DATA_WIDTH - 1:0] lsu_addr_reg;
+reg [LSU_MEM_DATA_WIDTH - 1:0] lsu_addr_reg_next;
+
+reg [`NOC_DATA_WIDTH - 1:0] flit_to_send;
+
+assign lsu_rd_data = data_buffer[LSU_MEM_DATA_WIDTH - 1:0];
+assign noc2_filter_data = flit_to_send;
+
+reg is_new_flit;
+reg is_new_flit_next;
+
+// update variables
+always @( * ) begin
+    noc2_filter_val = 1'b0;
+    noc3_filter_rdy = 1'b0;
+    lsu_ack = 1'b0;
+    flit_to_send = 64'b0;
+
+    lsu_tag_resp_next = lsu_tag_resp;
+    bits_seen_next = bits_seen;
+    flit_count_next = flit_count;
+    total_flits_to_send_next = total_flits_to_send;
+    total_flits_to_see_next = total_flits_to_see;
+    data_buffer_next = data_buffer;
+    lsu_addr_reg_next = lsu_addr_reg;
+    lsu_tag_req_reg_next = lsu_tag_req;
+    is_new_flit_next = is_new_flit;
+
+    case (state)
+        STATE_IDLE: begin
+            bits_seen_next = 11'd0;
+            flit_count_next = 6'd0;
+            total_flits_to_send_next = 6'd0;
+            total_flits_to_see_next = 6'd0;
+            data_buffer_next = 2048'd0;
+
+            if (lsu_wr_en) begin
+                is_new_flit_next = 1'b1;
+                total_flits_to_send_next = 4'd3;
+            end
+            else if (lsu_rd_en) begin
+                total_flits_to_see_next = MAX_PKT_LEN - 2; // 2 fewer header packets
+            end
+        end
+        STATE_WR_SAVE_DATA: begin
+            lsu_ack = 1'b1;
+            if (bits_seen == 0) begin
+                lsu_tag_req_reg_next = lsu_tag_req;
+                lsu_addr_reg_next = lsu_addr;
+            end
+
+            data_buffer_next[VGPR_BUS_WIDTH - LSU_MEM_DATA_WIDTH - 1: 0] = data_buffer[VGPR_BUS_WIDTH - 1:LSU_MEM_DATA_WIDTH];
+            data_buffer_next[VGPR_BUS_WIDTH - 1:VGPR_BUS_WIDTH - LSU_MEM_DATA_WIDTH] = lsu_wr_data;
+
+            bits_seen_next = bits_seen + 6'd32;
+        end
+        STATE_WR_WAIT_DATA: begin
+            if (is_new_flit) begin
+                total_flits_to_send_next = total_flits_to_send + 1;
+            end
+
+            is_new_flit_next = ~is_new_flit;
+        end
+        STATE_WR_SLIDE_DATA: begin
+            data_buffer_next[VGPR_BUS_WIDTH - LSU_MEM_DATA_WIDTH - 1: 0] = data_buffer[VGPR_BUS_WIDTH - 1:LSU_MEM_DATA_WIDTH];
+            
+            bits_seen_next = bits_seen + 6'd32;
+        end
+        STATE_WR_SEND_FLIT_ONE: begin
+            if (filter_noc2_rdy) begin
+                noc2_filter_val = 1'b1;
+                flit_count_next = flit_count + 1'b1;
+                flit_to_send[`MSG_LENGTH] = total_flits_to_send - 1;
+                flit_to_send[MSG_MSHRID_HI - 2:MSG_MSHRID_LO] = lsu_tag_req_reg;
+            end
+        end
+        STATE_WR_SEND_FLIT_TWO: begin
+            if (filter_noc2_rdy) begin
+                noc2_filter_val = 1'b1;
+                flit_count_next = flit_count + 1'b1;
+                flit_to_send[`MSG_ADDR_] = {16'b0, lsu_addr_reg};
+            end
+        end
+        STATE_WR_SEND_FLIT_THREE: begin
+            if (filter_noc2_rdy) begin
+                noc2_filter_val = 1'b1;
+                flit_count_next = flit_count + 1'b1;
+             end
+        end
+        STATE_WR_SEND_FLIT_DATA: begin
+            if (filter_noc2_rdy) begin
+                noc2_filter_val = 1'b1;
+                flit_count_next = flit_count + 1'b1;
+                flit_to_send = data_buffer[`NOC_DATA_WIDTH:0];
+                data_buffer_next[VGPR_BUS_WIDTH - 1 - `NOC_DATA_WIDTH:0] = data_buffer[VGPR_BUS_WIDTH - 1:`NOC_DATA_WIDTH];
+            end
+
+            if (flit_count >= total_flits_to_send - 1 && filter_noc2_rdy) begin
+                flit_count_next = 6'd0;
+            end
+        end
+        STATE_WR_ACK_WAIT: begin
+            noc3_filter_rdy = 1'b1;
+            if (filter_noc3_val) begin
+                lsu_tag_resp_next = filter_noc3_data[MSG_MSHRID_HI - 2:MSG_MSHRID_LO];
+            end
+        end
+        STATE_RD_SEND_FLIT_ONE: begin
+            if (filter_noc2_rdy) begin
+                noc2_filter_val = 1'b1;
+                flit_count_next = flit_count + 1'b1;
+                flit_to_send[`MSG_LENGTH] = 8'd3;
+                flit_to_send[MSG_MSHRID_HI - 2:MSG_MSHRID_LO] = lsu_tag_req;
+                lsu_addr_reg_next = lsu_addr;
+            end
+        end
+        STATE_RD_SEND_FLIT_TWO: begin
+            if (filter_noc2_rdy) begin
+                noc2_filter_val = 1'b1;
+                flit_count_next=  flit_count + 1'b1;
+                flit_to_send[`MSG_ADDR_] = {16'b0, lsu_addr_reg};
+            end
+        end
+        STATE_RD_SEND_FLIT_THREE: begin
+            if (filter_noc2_rdy) begin
+                noc2_filter_val = 1'b1;
+                flit_count_next = 6'd0;
+            end
+        end
+        STATE_RD_RESP_HEAD: begin
+            noc3_filter_rdy = 1'b1;
+            if (filter_noc3_val) begin
+                lsu_tag_resp_next = filter_noc3_data[MSG_MSHRID_HI - 2:MSG_MSHRID_LO];
+                flit_count_next = flit_count + 1'b1;
+            end
+        end
+        STATE_RD_RESP_DATA: begin
+            noc3_filter_rdy = 1'b1;
+            if (filter_noc3_val) begin
+                data_buffer_next[VGPR_BUS_WIDTH - 1 - `NOC_DATA_WIDTH:0] = data_buffer[VGPR_BUS_WIDTH - 1:`NOC_DATA_WIDTH];
+                data_buffer_next[VGPR_BUS_WIDTH - 1:VGPR_BUS_WIDTH - `NOC_DATA_WIDTH] = filter_noc3_data;
+                flit_count_next = flit_count + 1'b1;
+            end
+        end
+        STATE_RD_SEND_DATA: begin
+            lsu_ack = 1'b1;
+        end
+        STATE_RD_WAIT_TO_SEND: begin
+            data_buffer_next[VGPR_BUS_WIDTH - 1 - LSU_MEM_DATA_WIDTH:0] = data_buffer[VGPR_BUS_WIDTH - 1:LSU_MEM_DATA_WIDTH];
+        end
+        default: begin
+        end
+    endcase
+end
+
+// next state logic
+always @( * ) begin
+    state_next = state;
+
+    case (state)
+        STATE_IDLE: begin
+            if (lsu_wr_en) begin
+                state_next = STATE_WR_SAVE_DATA;
+            end
+            else if (lsu_rd_en) begin
+                state_next = STATE_RD_SEND_FLIT_ONE;
+            end
+        end
+        STATE_WR_SAVE_DATA: begin
+            state_next = STATE_WR_WAIT_DATA;
+        end
+        STATE_WR_WAIT_DATA: begin
+            if (lsu_wr_en) begin
+                state_next = STATE_WR_SAVE_DATA;
+            end
+            else begin
+                state_next = STATE_WR_SLIDE_DATA;
+            end
+        end
+        STATE_WR_SLIDE_DATA: begin
+            if (bits_seen >= VGPR_BUS_WIDTH - LSU_MEM_DATA_WIDTH) begin
+                state_next = STATE_WR_SEND_FLIT_ONE;
+            end
+        end
+        STATE_WR_SEND_FLIT_ONE: begin
+            if (filter_noc2_rdy) begin
+                state_next = STATE_WR_SEND_FLIT_TWO;
+            end
+        end
+        STATE_WR_SEND_FLIT_TWO: begin
+            if (filter_noc2_rdy) begin
+                state_next = STATE_WR_SEND_FLIT_THREE;
+            end
+        end
+        STATE_WR_SEND_FLIT_THREE: begin
+            if (filter_noc2_rdy) begin
+                state_next = STATE_WR_SEND_FLIT_DATA;
+            end
+        end
+        STATE_WR_SEND_FLIT_DATA: begin
+            if (flit_count >= total_flits_to_send - 1 && filter_noc2_rdy) begin
+                state_next = STATE_WR_ACK_WAIT;
+            end
+        end
+        STATE_WR_ACK_WAIT: begin
+            if (filter_noc3_val) begin
+                state_next = STATE_IDLE;
+            end
+        end
+        STATE_RD_SEND_FLIT_ONE: begin
+            if (filter_noc2_rdy) begin
+                state_next = STATE_RD_SEND_FLIT_TWO;
+            end
+        end
+        STATE_RD_SEND_FLIT_TWO: begin
+            if (filter_noc2_rdy) begin
+                state_next = STATE_RD_SEND_FLIT_THREE;
+            end
+        end
+        STATE_RD_SEND_FLIT_THREE: begin
+            if (filter_noc2_rdy) begin
+                state_next = STATE_RD_RESP_HEAD;
+            end
+        end
+        STATE_RD_RESP_HEAD: begin
+            if (filter_noc3_val) begin
+                state_next = STATE_RD_RESP_DATA;
+            end
+        end
+        STATE_RD_RESP_DATA: begin
+            if (filter_noc3_val && flit_count >= total_flits_to_see - 1) begin
+                state_next = STATE_RD_SEND_DATA;
+            end
+        end
+        STATE_RD_SEND_DATA: begin
+            state_next = STATE_RD_WAIT_TO_SEND;
+        end
+        STATE_RD_WAIT_TO_SEND: begin
+            if (lsu_rd_en) begin
+                state_next = STATE_RD_SEND_DATA;
+            end
+            else begin
+                state_next = STATE_IDLE;
+            end
+        end
+        default: begin
+            state_next = state;
+        end
+    endcase
+end
+
+// update registers
+always @(posedge clk) begin
+    if (rst) begin
+        state <= STATE_IDLE;
+        bits_seen <= 11'd0;
+        total_flits_to_send <= 6'd0;
+        total_flits_to_see <= 6'd0;
+        flit_count <= 6'd0;
+        data_buffer <= 2048'd0;
+        lsu_tag_resp <= 6'd0;
+        lsu_addr_reg <= 32'd0;
+        lsu_tag_req_reg <= 6'd0;
+        is_new_flit <= 1'b0;
+    end
+    else begin
+        state <= state_next;
+        bits_seen <= bits_seen_next;
+        total_flits_to_send <= total_flits_to_send_next;
+        total_flits_to_see <= total_flits_to_see_next;
+        flit_count <= flit_count_next;
+        data_buffer <= data_buffer_next;
+        lsu_tag_resp <= lsu_tag_resp_next;
+        lsu_addr_reg <= lsu_addr_reg_next;
+        lsu_tag_req_reg <= lsu_tag_req_reg_next;
+        is_new_flit <= is_new_flit_next;
+    end
+end
+
+endmodule
---
+++ src/verilog/rtl/fpga/neko.v
@@ -0,0 +1,128 @@
+module neko #
+(
+	parameter integer NEKO_AXI_DATA_WIDTH	= 32,
+	parameter integer NEKO_AXI_ADDR_WIDTH	= 11
+)
+(
+    input                                   chipset_clk,
+    input                                   rst_n,
+
+    input                                   noc2_in_val,
+    input       [`NOC_DATA_WIDTH-1:0]       noc2_in_data,
+    output                                  noc2_in_rdy,
+
+    output                                  noc3_out_val,
+    output      [`NOC_DATA_WIDTH-1:0]       noc3_out_data,
+    input                                   noc3_out_rdy,
+
+    output                                  noc2_out_val,
+    output      [`NOC_DATA_WIDTH-1:0]       noc2_out_data,
+    input                                   noc2_out_rdy,
+
+    input                                   noc3_in_val,
+    input       [`NOC_DATA_WIDTH-1:0]       noc3_in_data,
+    output                                  noc3_in_rdy
+
+);
+
+
+	wire  neko_axi_aclk;
+	wire  neko_axi_aresetn;
+	wire [NEKO_AXI_ADDR_WIDTH-1 : 0] neko_axi_awaddr;
+	wire [2 : 0] neko_axi_awprot;
+	wire  neko_axi_awvalid;
+	wire  neko_axi_awready;
+	wire [NEKO_AXI_DATA_WIDTH-1 : 0] neko_axi_wdata;
+	wire [(NEKO_AXI_DATA_WIDTH/8)-1 : 0] neko_axi_wstrb;
+	wire  neko_axi_wvalid;
+	wire  neko_axi_wready;
+	wire [1 : 0] neko_axi_bresp;
+	wire  neko_axi_bvalid;
+	wire  neko_axi_bready;
+	wire [NEKO_AXI_ADDR_WIDTH-1 : 0] neko_axi_araddr;
+	wire [2 : 0] neko_axi_arprot;
+	wire  neko_axi_arvalid;
+	wire  neko_axi_arready;
+	wire [NEKO_AXI_DATA_WIDTH-1 : 0] neko_axi_rdata;
+	wire [1 : 0] neko_axi_rresp;
+	wire  neko_axi_rvalid;
+	wire  neko_axi_rready;
+
+    assign neko_axi_aclk = chipset_clk;
+    assign neko_axi_aresetn = rst_n;
+
+noc_axilite_bridge_neko     noc_neko_bridge   (
+    .clk                    (chipset_clk        ),
+    .rst                    (~rst_n             ),      // TODO: rewrite to positive ?
+           
+    .splitter_bridge_val    (noc2_in_val   ),
+    .splitter_bridge_data   (noc2_in_data  ),
+    .bridge_splitter_rdy    (noc2_in_rdy   ),   // CRAZY NAMING !
+
+    .bridge_splitter_val    (noc3_out_val   ),
+    .bridge_splitter_data   (noc3_out_data  ),
+    .splitter_bridge_rdy    (noc3_out_rdy   ),   // CRAZY NAMING !
+       
+    //axi lite signals             
+    //write address channel
+    .m_axi_awaddr        (neko_axi_awaddr),
+    .m_axi_awvalid       (neko_axi_awvalid),
+    .m_axi_awready       (neko_axi_awready),
+
+    //write data channel
+    .m_axi_wdata         (neko_axi_wdata),
+    .m_axi_wstrb         (neko_axi_wstrb),
+    .m_axi_wvalid        (neko_axi_wvalid),
+    .m_axi_wready        (neko_axi_wready),
+
+    //read address channel
+    .m_axi_araddr        (neko_axi_araddr),
+    .m_axi_arvalid       (neko_axi_arvalid),
+    .m_axi_arready       (neko_axi_arready),
+
+    //read data channel
+    .m_axi_rdata         (neko_axi_rdata),
+    .m_axi_rresp         (neko_axi_rresp),
+    .m_axi_rvalid        (neko_axi_rvalid),
+    .m_axi_rready        (neko_axi_rready),
+
+    //write response channel
+    .m_axi_bresp         (neko_axi_bresp),
+    .m_axi_bvalid        (neko_axi_bvalid),
+    .m_axi_bready        (neko_axi_bready)
+);
+
+    compute_unit_fpga # (
+        .C_S_AXI_DATA_WIDTH(NEKO_AXI_DATA_WIDTH),
+        .C_S_AXI_ADDR_WIDTH(NEKO_AXI_ADDR_WIDTH)
+    ) compute_unit_fpga (
+        .S_AXI_ACLK(neko_axi_aclk),
+		.S_AXI_ARESETN(neko_axi_aresetn),
+		.S_AXI_AWADDR(neko_axi_awaddr),
+		.S_AXI_AWPROT(neko_axi_awprot),
+		.S_AXI_AWVALID(neko_axi_awvalid),
+		.S_AXI_AWREADY(neko_axi_awready),
+		.S_AXI_WDATA(neko_axi_wdata),
+		.S_AXI_WSTRB(neko_axi_wstrb),
+		.S_AXI_WVALID(neko_axi_wvalid),
+		.S_AXI_WREADY(neko_axi_wready),
+		.S_AXI_BRESP(neko_axi_bresp),
+		.S_AXI_BVALID(neko_axi_bvalid),
+		.S_AXI_BREADY(neko_axi_bready),
+		.S_AXI_ARADDR(neko_axi_araddr),
+		.S_AXI_ARPROT(neko_axi_arprot),
+		.S_AXI_ARVALID(neko_axi_arvalid),
+		.S_AXI_ARREADY(neko_axi_arready),
+		.S_AXI_RDATA(neko_axi_rdata),
+		.S_AXI_RRESP(neko_axi_rresp),
+		.S_AXI_RVALID(neko_axi_rvalid),
+		.S_AXI_RREADY(neko_axi_rready),
+
+        .noc2_filter_val(noc2_out_val),
+        .noc2_filter_data(noc2_out_data),
+        .filter_noc2_rdy(noc2_out_rdy),
+        .filter_noc3_val(noc3_in_val),
+        .filter_noc3_data(noc3_in_data),
+        .noc3_filter_rdy(noc3_in_rdy)
+    );
+endmodule
